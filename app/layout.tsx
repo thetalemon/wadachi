94173487f21a67674a4828f30e573fd113701fc8
thetalemon <sasami0314@gmail.com>
2026-09-03 10:41:05 +0900

    layout変更

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { ThemeProvider } from 'next-themes'
 import './globals.css'
 import { Suspense } from 'react'
 import Link from 'next/link'
+import { DeployButton } from '@/components/deploy-button'
 import { AuthButton } from '@/components/auth-button'
 import { ThemeSwitcher } from '@/components/theme-switcher'
 
@@ -42,6 +43,9 @@ export default function RootLayout({
               <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                 <div className="flex gap-5 items-center font-semibold">
                   <Link href={'/'}>Next.js Supabase Starter</Link>
+                  <div className="flex items-center gap-2">
+                    <DeployButton />
+                  </div>
                 </div>
                 <div className="flex items-center gap-3">
                   <Suspense>
@@ -53,6 +57,10 @@ export default function RootLayout({
             </nav>
 
             <main className="flex-1">{children}</main>
+
+            <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
+              <ThemeSwitcher />
+            </footer>
           </div>
         </ThemeProvider>
       </body>
